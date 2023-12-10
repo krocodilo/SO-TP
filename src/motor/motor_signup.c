f39effb70a5bb408b6a4fde0b7e187f1c030358a
krocodilo <dcruz98.exe@gmail.com>
2023-12-10 22:51:01 +0000

    motor_signup.c for functions related to clients initial communication

---
+++ src/motor/motor_signup.c
@@ -0,0 +1,100 @@
+#include "motor_signup.h"
+
+
+
+
+void waitForClientsSignUp(GameSettings gameSettings){
+    /*
+     * Wait for enough players to sign up
+     */
+
+    //cosntrucao da struct timeval para usar no select
+    struct timeval waitTime = {gameSettings.signupWindowDurationSeconds, 0};
+    fd_set read_fds;
+    int sval, hasTimedOut = 0;
+
+    printf("\n\nA espera de jogadores...\n\n");
+
+    while(game->nPlayers < MAX_PLAYERS &&
+          !(hasTimedOut && game->nPlayers >= gameSettings.minPlayers)
+        // sai se ja terminou a espera, mas entretanto ja tem o minimo de jogadores
+            ){
+
+        if(!hasTimedOut){
+            FD_ZERO(&read_fds);            //inicializa a watchlist
+            FD_SET(game->pipegen_fd, &read_fds); // add pipegen_fd ao conjunto watchlist
+
+            sval = select(
+                    game->pipegen_fd +1,
+                    &read_fds, NULL, NULL,
+                    &waitTime               // cada uso do select decrementa o waitTime
+            );
+            if(sval == 0){  // Timeout
+                if(game->nPlayers >= gameSettings.minPlayers){
+                    printf("\n\nO periodo de inscricoes terminou, existindo o minimo de jogadores.");
+                    break;
+                }
+                // else:
+                printf("\nO periodo de inscricoes terminou, sem o minimo de jogadores. Pretende"
+                       "esperar pelo minimo numero de jogadores? \n(yes/no, DEFAULT: yes) _>  ");
+
+                char resp[5];
+                fgets(resp, sizeof(resp), stdin);
+                resp[strcspn(resp, "\n")] = '\0';
+
+                if (strcmp(resp, "no") == 0)
+                    break;
+                hasTimedOut = 1;
+            }
+            else if(sval == -1)
+                perror("\n\nErro na espera de jogadores.");
+        }
+
+
+        // read pipe
+        if( receiveNewPlayer( &game->players[game->nPlayers] ) )
+            game->nPlayers++;
+    }
+}
+
+
+
+bool receiveNewPlayer(Player* newPlayer){
+    /*
+     * read a new player from the general pipe
+     */
+
+    char username[MAX_PLAYER_NAME];
+
+    if ( read(game->pipegen_fd, &username, sizeof(username)) != sizeof(username) ){
+        perror("\nERRO: foi recebida uma mensagem incompleta aquando tentativa de inscricao.\n");
+        return false;
+    }
+
+    strcpy(newPlayer->username, username);
+
+    newPlayer->pipe_fd = open(newPlayer->username, O_RDWR);
+
+    if (newPlayer->pipe_fd == -1){ //se o pipe do cliente der erro a abrir!
+        fprintf(stderr, "\nERRO ao abrir o pipe com o cliente %s\n", newPlayer->username);
+        return false;
+        // opcionalmente, enviar um sinal ao cliente para informar da impossibilidade
+    }
+
+    // Informar o cliente do sucesso
+//    sm.wait = 1;
+//    strcpy(sm.message, "\nA espera de mais jogadores...\n");
+//    write(p->pipe_fd, &sm, sizeof(sm));  //envia para os jogadores
+//
+//
+//    printf("Foi adicionado o jogador: %s\n", msg.message);
+//
+//    close(p->pipe_fd);
+//    p->pipe_fd = -1;    //-1 para indicar a funcao terminate que o pipe nao esta aberto
+//    p->next = NULL;
+//
+//    p->continuaPlayer = 1
+//    p->isOut = 0; //0 -> esta dentro do campeonato
+
+    return true;
+}
---
+++ src/motor/motor_signup.h
@@ -0,0 +1,24 @@
+#ifndef SO_TP_MOTOR_SIGNUP_H
+#define SO_TP_MOTOR_SIGNUP_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include "data_structs.h"
+#include "../common/constants.h"
+
+#include "global_variables.h"
+Game* game;     // External global variable, declared in global_variables.h
+
+
+void waitForClientsSignUp(GameSettings gameSettings);
+bool receiveNewPlayer(Player* newPlayer);
+
+
+
+#endif //SO_TP_MOTOR_SIGNUP_H
